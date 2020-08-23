de77999296d1c018ac9094627afdd2febc817bf2
Dansp Win10 <dsp102009@gmail.com>
2020-08-23 11:39:52 -0300

    main for automotive

---
+++ automotive/src/main/java/br/com/platis/autocar/MainActivity.kt
@@ -0,0 +1,39 @@
+package br.com.platis.autocar
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                // Log and toast
+                // val msg = getString(R.string.msg_token_fmt, token)
+                Log.d(TAG, token.toString())
+                Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+            })
+    }
+
+    companion object {
+
+        private const val TAG = "MainActivity"
+    }
+}
